7a28fbea44e298cc069f06428fb8bd2e57824e0e
Sean Tremblay <seantremblay@utexas.edu>
2019-02-12 21:02:16 -0600

    Add files via upload

--- hw1b.py
+++ hw1b.py
@@ -24,41 +24,48 @@ Xtest = item_dataset['Xtest']
 Ytrain = item_dataset['Ytrain']
 Ytest = item_dataset ['Ytest']
 
-X = dataset[:,0:99]
-Y = dataset[:, 100]
-
-xt = X[1]
-xl = X[:,1]
 
 plt.figure(1)
-plt.title("Scatter Plot")
+plt.title("Scatter Plot 1")
 
-plt.scatter(Xtrain, Ytrain, 'r')
 
-plt.scatter(Xtest, Ytest, 'g')
+Xtrain_transpose = Xtrain.transpose()
+Xtest_transpose = Xtest.transpose()
+
+plt.scatter(Xtrain, Ytrain, c='r', label= 'train')
+
+plt.scatter(Xtest, Ytest, c='y', label= 'test')
 
 plt.legend()
-
 # PART B
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
-reg = LinearRegression().fit(Xtrain, Ytrain)
+
+reg = LinearRegression(fit_intercept=True).fit(Xtrain, Ytrain)
 
 R_squared_train = reg.score(Xtrain, Ytrain)
 
-reg2 = LinearRegression().predict(Xtest)
+reg2 = reg.predict(Xtest)
 
-reg2.score(Xtest, Ytest)
+test_score_R2 = reg.score(reg2, Ytest)
+mse_lin = mean_squared_error(reg2, Ytest)
+print(test_score_R2)
 plt.figure(2)
 
 
-plt.scatter(Xtrain, Ytrain, 'r')
+plt.scatter(Xtrain, Ytrain, c='r', label= 'train')
 
-plt.scatter(Xtest, Ytest, 'g')
+plt.scatter(Xtest, Ytest, c='y', label= 'test')
 
-plt.plot(Xtrain,reg2.predict(Xtrain),color='k')
+plt.plot(Xtrain,reg.predict(Xtrain),color='k')
 
-plt.plot(Xtest,reg2.predict(Xtest),color='b')
+plt.plot(Xtest,reg.predict(Xtest),color='b')
+
+plt.show()
+plt.title("Scatter plot of linear regression line")
+plt.legend(loc='upper left')
+print("Betas are:" + str(reg.coef_))
 
 # Need to calulate average MSE^2
 
@@ -67,26 +74,50 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn import linear_model
 plt.figure(3)
 
+plt.scatter(Xtrain, Ytrain, c ='r')
 
-plt.scatter(Xtrain, Ytrain, 'r')
-
-plt.scatter(Xtest, Ytest, 'g')
+plt.scatter(Xtest, Ytest, c= 'y')
 
 
-def polyFeature(degree, X, x_test, Y, poly):
+
+def polyFeature(degree, X, x_test, Y, poly, colors):
     X_ = poly.fit_transform(X)
-    predict_ = poly.fit_transform(x_test)
+
+    predictp = poly.fit_transform(x_test)
     clf = linear_model.LinearRegression()
     clf.fit(X_, Y)
-    print clf.predict(predict_)
-    return clf.predict(predict_)
 
+    return clf.predict(predictp), clf.coef_
 
+def MLR(X, y, regressor):
+    regressor.fit(X, y)
+    return regressor
+
+# import statsmodels.api as sm
+#
+# def OLS_mult(Y, X):
+#     model = sm.OLS(Y,X)
+#     result = model.fit()
+#     print result.summary()
+
+colors= ['b', 'g','r','c','m', 'y', 'k']
 # PART 3
-for i in range(2,7):
-    poly = PolynomialFeatures(degree=i)
-    prediction = polyFeature(i,Xtrain,Xtest,Ytrain, poly)
-    plt.plot(Xtest, prediction, color='k')
+for i in range(2,8):
+    regressor = LinearRegression()
+    X_evaluate = poly_feature(Xtrain, i)
+    #OLS_mult(Ytrain, X_evaluate)
+    reg_mult = MLR(X_evaluate, Ytrain, regressor=regressor)
+    coef = reg_mult.coef_
+    X_evaluate_test = poly_feature(Xtest, i)
+
+    y_value = np.dot( X_evaluate_test, coef.T)
+    #X_evaluate_test = poly_feature(Xtest, i)
+    #y_evaluate_test = reg_mult.predict(X_evaluate_test)
+    plt.plot(Xtest, regressor.predict(Xtest), color = colors[i])
+
+
+plt.legend(loc='upper left')
+print("0")
 
 
 
